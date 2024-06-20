4351932e876eac119e4729c2438a5ac5d28c0632
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-06-21 01:02:27 +0200

    Started implementing a base model handling class to reduce the VAO boilerplate while still being minimal in design

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # trading-visualizer
 A tool written in OpenGL and PyGame that can read in historic tick data and interactively plot it 
+
+## Notes
+### Version 0 instead of Version 330 in OpenGL
+The order of the creation of the objects is important, the following works:
+```python
+def setup_pygame_and_opengl(self):
+    pg.init()
+
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+    pg.display.gl_set_attribute(
+        pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+    )
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_FORWARD_COMPATIBLE_FLAG, True)
+
+    self.pg_window: pg.Surface = pg.display.set_mode(
+        (1600, 900), flags=pg.OPENGL | pg.DOUBLEBUF
+    )
+
+    self.ctx: Context = mgl.create_context()
+    self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
+```
+while the following does not:
+```python
+def setup_pygame_and_opengl(self):
+    pg.init()
+
+    self.pg_window: pg.Surface = pg.display.set_mode(
+        (1600, 900), flags=pg.OPENGL | pg.DOUBLEBUF
+    )
+
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+    pg.display.gl_set_attribute(
+        pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+    )
+    pg.display.gl_set_attribute(pg.GL_CONTEXT_FORWARD_COMPATIBLE_FLAG, True)
+
+
+    self.ctx: Context = mgl.create_context()
+    self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
+```
+
+### No window appears
+Check if you're actually getting the event queue every frame, even if you're not doing anything significant with it.
+```python
+def handle_events(self):
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            self.is_running = False
+```
--- src/graphics_engine.py
+++ src/graphics_engine.py
@@ -1,6 +1,8 @@
 import os
+from abc import ABC, abstractmethod
 
 import moderngl as mgl
+import numpy as np
 from moderngl import Buffer, Context, Program, VertexArray  # noqa: F401
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
@@ -13,7 +15,7 @@ class GraphicsEngine:
 
         self.is_running = True
 
-    def setup_pygame_and_opengl(self):
+    def setup_pygame_and_opengl(self) -> None:
         pg.init()
 
         self.clock = pg.time.Clock()
@@ -32,15 +34,15 @@ class GraphicsEngine:
         self.ctx: Context = mgl.create_context()
         self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
 
-    def handle_events(self):
+    def handle_events(self) -> None:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.is_running = False
 
-    def update(self):
+    def update(self) -> None:
         pass
 
-    def render(self):
+    def render(self) -> None:
         self.ctx.clear(1.0, 0.0, 1.0)
 
         pg.display.flip()
@@ -54,4 +56,52 @@ class GraphicsEngine:
         while self.is_running:
             self.iteration()
 
-            self.clock.tick(15.0)
+            self.clock.tick(60.0)
+
+
+class BaseObject(ABC):
+    def __init__(self, app: GraphicsEngine):
+        self.app: GraphicsEngine = app
+        self.ctx = self.app.ctx
+
+    def render(self) -> None:
+        self.vao.render()
+
+    def create_vao(self) -> None:
+        self.vbo = self.ctx.buffer(self.get_vertex_data())
+        self.program = self.get_program()
+
+        self.vao = self.ctx.vertex_array(
+            self.program,
+            [
+                (self.vbo, self.get_buffer_format(), *self.get_attributes()),
+            ],
+        )
+
+    @abstractmethod
+    def update(self) -> None: ...
+
+    @abstractmethod
+    def get_buffer_format(self) -> str: ...
+
+    @abstractmethod
+    def get_attributes(self) -> list[str]: ...
+
+    @abstractmethod
+    def get_vertex_data(self) -> np.ndarray: ...
+
+    def get_program(self) -> Program:
+        return self.ctx.program(
+            vertex_shader=self.get_vertex_shader(),
+            fragment_shader=self.get_fragment_shader(),
+        )
+
+    @abstractmethod
+    def get_vertex_shader(self) -> str: ...
+
+    @abstractmethod
+    def get_fragment_shader(self) -> str: ...
+
+
+class QuadBaseObject(ABC):
+    pass
