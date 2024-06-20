4ebba01d9b33aea9ca3419d8937b6ec678f1042f
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-06-21 00:32:41 +0200

    Added class init code

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.graphics_engine import GraphicsEngine
+
+
+def main() -> None:
+    graphics_engine = GraphicsEngine()
+    graphics_engine.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/graphics_engine.py
@@ -0,0 +1,49 @@
+import os
+
+import moderngl as mgl
+from moderngl import Buffer, Context, Program, VertexArray  # noqa: F401
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import pygame as pg  # noqa: E402
+
+
+class GraphicsEngine:
+    def __init__(self):
+        self.setup_pygame_and_opengl()
+
+        self.is_running = True
+
+    def setup_pygame_and_opengl(self):
+        pg.init()
+
+        self.pg_window: pg.Surface = pg.display.set_mode(
+            (1600, 900), flags=pg.OPENGL | pg.DOUBLEBUF
+        )
+        self.clock = pg.time.Clock()
+
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(
+            pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+        )
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_FORWARD_COMPATIBLE_FLAG, True)
+
+        self.ctx: Context = mgl.create_context()
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
+
+    def update(self): ...
+
+    def render(self):
+        self.ctx.clear(1.0, 0.0, 1.0)
+
+        pg.display.flip()
+
+    def iteration(self) -> None:
+        self.update()
+        self.render()
+
+    def run(self) -> None:
+        while self.is_running:
+            self.iteration()
+
+            self.clock.tick(60.0)
